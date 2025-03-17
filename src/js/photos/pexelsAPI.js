561e677add2d45c148226e782a44eb185905e332
Halyna Moldovianu <Beorgeos@gmail.com>
2025-03-17 14:19:48 +0100

    photos

---
+++ src/js/photos/pexelsAPI.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '563492ad6f9170000100000108dc2880626e4436b3634ce1cf6b4d74';
+
+axios.defaults.headers.common['Authorization'] = API_KEY;
+axios.defaults.baseURL = 'https://api.pexels.com/v1';
+axios.defaults.params = {
+  color: 'black',
+  orientation: 'landscape',
+  per_page: 20,
+};
+
+export const fetchPhotos = async function (query) {
+  const { data } = await axios.get(`/search?query=${query}`);
+
+  return data;
+};
